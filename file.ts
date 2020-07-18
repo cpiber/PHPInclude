e990c2e324bdb1d155c8c060b0289ae7b885cd12
Constantin Piber <cp.piber@gmail.com>
2020-07-18 13:12:27 +0200

    Expose all fileTypes for future extensions (ts+js)

---
+++ file.ts
@@ -0,0 +1,5 @@
+import GenericFile from './src/fileTypes/file';
+import PhpFile from './src/fileTypes/php';
+import WebpackFile from './src/fileTypes/webpack';
+import JsFile from './src/fileTypes/js';
+export { GenericFile, PhpFile, WebpackFile, JsFile };
