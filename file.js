30daa728e8bfa894e668a494cd033d0053299d82
Constantin Piber <constantin.piber@student.tugraz.at>
2020-07-23 13:58:44 +0200

    Expose filetypes for future extensions (js+ts)

--- file.js
+++ file.js
@@ -1,2 +1,7 @@
 // Shorthand for extensions
-module.exports = exports = require('./build/fileTypes/file').default;
+module.exports = exports = {
+  GenericFile: require('./build/fileTypes/file').default,
+  PhpFile: require('./build/fileTypes/php').default,
+  WebpackFile: require('./build/fileTypes/webpack').default,
+  JsFile: require('./build/fileTypes/js').default
+}
---
+++ file.ts
@@ -0,0 +1,6 @@
+// Shorthand for extensions
+import GenericFile from './src/fileTypes/file';
+import PhpFile from './src/fileTypes/php';
+import WebpackFile from './src/fileTypes/webpack';
+import JsFile from './src/fileTypes/js';
+export { GenericFile, PhpFile, WebpackFile, JsFile };
