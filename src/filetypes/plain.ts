c46de788ec3f5b70b93635571b710cd5882e8488
Constantin Piber <cp.piber@gmail.com>
2022-02-14 10:36:34 +0100

    Add TODO

--- src/filetypes/plain.ts
+++ src/filetypes/plain.ts
@@ -3,6 +3,7 @@ import { BuildFile } from './file';
 class PlainFile extends BuildFile {
   async process(filename: string, contents: string | Buffer) {
     contents = contents.toString();
+    // TODO: content might contain $ -> escape!
     this.setContent(filename, `return ${JSON.stringify(contents)};`);
     return true;
   }
