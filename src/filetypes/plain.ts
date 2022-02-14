8c25ba228bbab37b777acec51f65056f92acf590
Constantin Piber <cp.piber@gmail.com>
2022-02-14 11:17:26 +0100

    Plain text: update string escaping

--- src/filetypes/plain.ts
+++ src/filetypes/plain.ts
@@ -3,9 +3,21 @@ import { BuildFile } from './file';
 class PlainFile extends BuildFile {
   async process(filename: string, contents: string | Buffer) {
     contents = contents.toString();
-    // TODO: content might contain $ -> escape!
-    this.setContent(filename, `return ${JSON.stringify(contents)};`);
+    this.setContent(filename, `return ${this.stringify(contents)};`);
     return true;
+  }
+
+  /**
+   * Escape string for embedding, single-quotes version
+   * @see https://www.npmjs.com/package/stringify-object
+   * @see https://github.com/yeoman/stringify-object/pull/72
+   * @param input to stringify
+   * @returns stringified version
+   */
+  private stringify(input: string) {
+    input = input.replace(/\\/g, '\\\\');
+    input = input.replace(/'/g, '\\\'');
+    return `'${input}'`;
   }
 }
 
