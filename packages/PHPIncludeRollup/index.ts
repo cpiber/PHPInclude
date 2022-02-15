1c4ec4c464b5ad11d9485a341d455bb73cc4a297
Constantin Piber <cp.piber@gmail.com>
2022-02-15 18:38:13 +0100

    rollup plugin fix and expand reading config files

--- packages/PHPIncludeRollup/index.ts
+++ packages/PHPIncludeRollup/index.ts
@@ -1,6 +1,7 @@
 import { ok, strictEqual } from 'assert';
+import { stat } from 'fs/promises';
 import { resolve } from 'path';
-import { error, PlainFile } from 'phpinclude/dev';
+import { debugLog, error, PlainFile } from 'phpinclude/dev';
 import { rollup } from 'rollup';
 // @ts-ignore
 import loadConfigFile from 'rollup/dist/loadConfigFile';
@@ -8,18 +9,26 @@ import loadConfigFile from 'rollup/dist/loadConfigFile';
 class RollupFile extends PlainFile {
   async process(filename: string, contents: string | Buffer) {
     let opt: import('rollup').MergedRollupOptions = { output: [] };
-    try {
-      const { options } = await loadConfigFile('rollup.config.js');
-      strictEqual(options.length, 1, 'Exactly 1 configuration has to be provided');
-      opt = options[0];
-    } catch (e) {
-      console.warn('WARNING: Could not load config from file:');
-      console.warn((e as any).message || e);
+    for (const pth of ['rollup.config.js', 'rollup.config.mjs', 'rollup.config.cjs']) {
+      try {
+        await stat(pth);
+      } catch (e) {
+        debugLog(`Skipped config file \`${pth}\``);
+        continue;
+      }
+      try {
+        const { options } = await loadConfigFile(resolve(pth));
+        strictEqual(options.length, 1, 'Exactly 1 configuration has to be provided');
+        opt = options[0];
+      } catch (e) {
+        console.warn(`WARNING: Could not load config from file \`${pth}\`:`);
+        console.warn((e as any).message || e);
+      }
     }
     try {
       opt.input = resolve(filename);
       const { generate } = await rollup(opt);
-      ok(opt.output.length <= 1, 'Only 1 output may be provided');
+      ok(opt.output.length <= 1, 'Only 1 output option may be provided');
       const { output } = await generate(opt.output[0] || {});
       strictEqual(output.length, 1);
       this.setContent(filename, `return ${this.stringify(output[0].code)};`);
